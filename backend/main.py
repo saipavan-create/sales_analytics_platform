8fc5e6ec7589ab8f4ad5ee09e9cba9c5086b9485
Saipavan Varagana <saipavan_v@aidenai.com>
2026-09-20 16:58:40 -0500

    Update cors to correct

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://salesanalyticslearning.vercel.app",
+        "https://sales-analytics-platform-ashen.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
